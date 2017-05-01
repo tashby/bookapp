48294387e6652b4416485cd8aae3f45b7389c95e
Tashby <tashby2714@gmail.com>
2017-05-01 09:58:28 -0400

    video 2 44min mark

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem 'simple_form', '~> 3.4'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+gem 'devise', '~> 4.2', '>= 4.2.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,11 +9,13 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.new
+    @book = current_user.books.build
+    @categories = Category.all.map{ |c| [c.name, c.id] }
   end
 
   def create
-    @book = Book.new(book_params)
+    @book = current_user.books.build(book_params)
+    @book.category_id = params[:category_id]
 
     if @book.save
       redirect_to root_path
@@ -23,10 +25,11 @@ class BooksController < ApplicationController
   end
 
   def edit
-
+    @categories = Category.all.map{ |c| [c.name, c.id] }
   end
 
   def update
+    @book.category_id = params[:category_id]
     if @book.update(book_params)
       redirect_to book_path(@book)
     else
@@ -43,7 +46,7 @@ class BooksController < ApplicationController
   private
 
     def book_params
-      params.require(:book).permit(:title, :description, :author)
+      params.require(:book).permit(:title, :description, :author, :category_id)
     end
 
     def find_book
